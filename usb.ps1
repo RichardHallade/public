8bb07e6953482b7d0197338e595eee44161ce12f
RichardHallade <131862158+RichardHallade@users.noreply.github.com>
2024-06-11 15:17:24 +0200

    Create usb.ps1

---
+++ usb.ps1
@@ -0,0 +1,25 @@
+#Changes Background  
+#URL For the Image of your choice (Wanna Cry Ransomware Background)
+$wallpaperurl = "https://raw.githubusercontent.com/RichardHallade/public/de7a08056b5f34decd4271b34eef251581440b60/hacking.png"
+
+Invoke-WebRequest $wallpaperurl -OutFile C:\temp\test.jpg
+
+$setwallpapersrc = @"
+using System.Runtime.InteropServices;
+
+public class Wallpaper
+{
+  public const int SetDesktopWallpaper = 20;
+  public const int UpdateIniFile = 0x01;
+  public const int SendWinIniChange = 0x02;
+  [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+  private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+  public static void SetWallpaper(string path)
+  {
+    SystemParametersInfo(SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange);
+  }
+}
+"@
+Add-Type -TypeDefinition $setwallpapersrc
+
+[Wallpaper]::SetWallpaper("C:\temp\test.jpg")
