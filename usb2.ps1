767e4ddda31ebbdb4b7a7dff7d893c49dfee4415
RichardHallade <131862158+RichardHallade@users.noreply.github.com>
2024-06-12 09:32:13 +0200

    Update usb2.ps1

--- usb2.ps1
+++ usb2.ps1
@@ -1,32 +1,28 @@
-# Définir l'URL de l'image
-$imageUrl = "https://raw.githubusercontent.com/RichardHallade/public/de7a08056b5f34decd4271b34eef251581440b60/hacking.png"
-
-# Définir le chemin où l'image sera sauvegardée localement
-$imagePath = "c:\temp\test.png"
-
-# Télécharger l'image
-Invoke-WebRequest -Uri $imageUrl -OutFile $imagePath
-
-# Définir le fond d'écran
-function Set-Wallpaper {
-    param (
-        [string]$Path
-    )
-    
-    Add-Type @"
-        using System;
-        using System.Runtime.InteropServices;
-        public class Wallpaper {
-            [DllImport("user32.dll", CharSet = CharSet.Auto)]
-            public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
-            public const int SPI_SETDESKWALLPAPER = 0x0014;
-            public const int SPIF_UPDATEINIFILE = 0x01;
-            public const int SPIF_SENDWININICHANGE = 0x02;
-        }
-"@
-    
-    [Wallpaper]::SystemParametersInfo(0x0014, 0, $Path, 0x01 -bor 0x02)
+# Sets Volume to max level
+$k = [Math]::Ceiling(100/2)
+$o = New-Object -ComObject WScript.Shell
+for ($i = 0; $i -lt $k; $i++) {
+    $o.SendKeys([char]175)
 }
 
-# Appeler la fonction pour définir le fond d'écran
-Set-Wallpaper -Path $imagePath
+# Sets up speech module
+$s = New-Object -ComObject SAPI.SpVoice
+
+# Set the voice (change 'Microsoft Zira Desktop - English (United States)' to a female voice on your system)
+$desiredVoice = 'Microsoft Zira Desktop - English (United States)'
+$availableVoices = $s.GetVoices()
+
+$voice = $availableVoices | Where-Object { $_.GetDescription() -eq $desiredVoice }
+
+if ($voice -eq $null) {
+    Write-Host "Voice '$desiredVoice' not found. Using the default voice."
+    $voice = $availableVoices[0]  # Utiliser la première voix par défaut
+}
+
+$s.Voice = $voice
+
+$s.Rate = -2
+$s.Speak("Wake up Neo")
+$s.Speak("The Matrix has you")
+$s.Speak("Follow the white rabbit")
+$s.Speak("Knock, knock, Neo.")
